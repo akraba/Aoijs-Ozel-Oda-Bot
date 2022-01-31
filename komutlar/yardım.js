ca50384fb21df39b06aace6e3945cc1b3b145199
Parzi <86732697+ParziDev@users.noreply.github.com>
2022-01-31 13:43:35 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name:"yardım",
+  aliases:["help"],
+  code:`
+  $color[BLACK]
+  $author[Yardım Menüsü;$userAvatar]
+  $description[
+**özel-oda kategori/kanal/kapat**: \`Özel Oda Sistemini Ayarlarsınız\`
+**özel-oda-kur**: \`Özel Oda Kurarsınız\`
+**özel-oda-sil**: \`Kurduğunuz Özel Odayı Silersiniz\`
+**çek**: \`Kurdugunuz Özel Odaya Özel Kişileri Çekersiniz\`
+**at**: \`Kurduğunuz Odadan Üyeleri Atarsınız\`]
+  $thumbnail[$authorAvatar]
+  `          
+}
